9de77a95bd74e78774d6c5cc9e4642c2d6a93b8d
Yuxuan Ren <renyuxuan1234@gmail.com>
2025-10-18 17:36:25 -0500

    first commit

--- black-box attack/main.py
+++ black-box attack/main.py
@@ -11,12 +11,55 @@ import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
 # matplotlib inline
-from zo_attack import *
+# from zo_attack import *
+from zo_attack import pgd_attack, zo_attack, r_attack, rzo_attack, imshow
+import zo_attack as _zo
 import cifar10_models as cm
 
+import os
+
 # CUDA usage
-use_cuda = True
-device = torch.device("cuda" if use_cuda else "cpu")
+
+use_cuda = torch.cuda.is_available()
+use_mps  = hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
+device   = torch.device("cuda" if use_cuda else ("mps" if use_mps else "cpu"))
+print("Using device:", device, "| CUDA avail:", torch.cuda.is_available())
+
+# keep zo_attack on the same device
+import zo_attack as _zo
+_zo.device = device
+
+data_root = os.path.join(os.path.dirname(__file__), "data")
+
+# CIFAR-10 stats
+CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
+CIFAR10_STD  = (0.2470, 0.2435, 0.2616)
+
+# data transforms
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+])
+
+# datasets (download once)
+trainset = torchvision.datasets.CIFAR10(
+    root=data_root, train=True, transform=transform_train, download=True
+)
+testset = torchvision.datasets.CIFAR10(
+    root=data_root, train=False, transform=transform_test, download=True
+)
+
+# (optional) loaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2, pin_memory=True)
+testloader  = torch.utils.data.DataLoader(testset,  batch_size=256, shuffle=False, num_workers=2, pin_memory=True)
+
 
 
 def main():
@@ -49,8 +92,9 @@ def main():
 
     transform_validation = transforms.Compose([transforms.ToTensor()])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform_validation)
+    # >>> MINIMAL FIX: use data_root and download=True <<<
+    trainset = torchvision.datasets.CIFAR10(root=data_root, train=True, transform=transform_train, download=True)
+    testset  = torchvision.datasets.CIFAR10(root=data_root, train=False, transform=transform_validation, download=True)
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=4)
     testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=4)
@@ -59,7 +103,9 @@ def main():
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     # Load model
-    model = cm.vgg11_bn(pretrained=True).to(device)
+    # model = cm.vgg11_bn(pretrained=True).to(device)
+    model = torch.hub.load('chenyaofo/pytorch-cifar-models',
+                       'cifar10_vgg11_bn', pretrained=True).to(device)
 
     # get some random training images
     dataiter = iter(testloader)
@@ -105,6 +151,7 @@ def main():
 
     for images, labels in dataiter:
         total += 1
+        # print("Processing image number: %s" % total)
         if total == 9933:
             # show image and label
             images = images.to(device)
@@ -132,20 +179,20 @@ def main():
             ori_images = ori_images.to(device)
             eta = images - ori_images
 
-            # ZO attack
-            images, i2, val_perturb_zo, time_perturb_2 = zo_attack(model, ori_images, labels)
-            labels = labels.to(device)
-            outputs = model(images)
+            # # ZO attack
+            # images, i2, val_perturb_zo, time_perturb_2 = zo_attack(model, ori_images, labels)
+            # labels = labels.to(device)
+            # outputs = model(images)
 
-            _, pre = torch.max(outputs.data, 1)
+            # _, pre = torch.max(outputs.data, 1)
 
-            total += 1
-            correct += (pre == labels).sum()
+            # total += 1
+            # correct += (pre == labels).sum()
 
-            imshow(torchvision.utils.make_grid(images.cpu().data, normalize=True), [classes[i] for i in pre])
-            distance = images.cpu() - ori_images.cpu()
-            print('ZO-attack: Distance of the attack example to original image:',
-                  np.linalg.norm(distance.cpu().data.numpy()))
+            # imshow(torchvision.utils.make_grid(images.cpu().data, normalize=True), [classes[i] for i in pre])
+            # distance = images.cpu() - ori_images.cpu()
+            # print('ZO-attack: Distance of the attack example to original image:',
+            #       np.linalg.norm(distance.cpu().data.numpy()))
 
             # Riemannian attack
             images, i3, val_perturb_r, time_perturb_3 = r_attack(model, ori_images, eta, labels)
@@ -182,7 +229,7 @@ def main():
     fig, ax = plt.subplots()
     plt.xscale('symlog')
     ax.plot(range(i1), val_perturb_pgd[:i1], 'r--', label='White-box PGD')
-    ax.plot(range(i2), val_perturb_zo[:i2], 'g--', label='Black-box ZO PGD')
+    # ax.plot(range(i2), val_perturb_zo[:i2], 'g--', label='Black-box ZO PGD')
     ax.plot(range(i3), val_perturb_r[:i3], 'k--',label='White-box Riemannian')
     ax.plot(range(i4), val_perturb_rzo[:i4], 'b:',label='Black-box ZO Riemannian')
     ax.set(xlabel='Number of iteration', ylabel='Loss value',
@@ -194,7 +241,7 @@ def main():
     fig, ax = plt.subplots()
     plt.xscale('symlog')
     ax.plot(time_perturb_1[:i1], val_perturb_pgd[:i1], 'r--', label='White-box PGD')
-    ax.plot(time_perturb_2[:i2], val_perturb_zo[:i2], 'g--', label='Black-box ZO PGD')
+    # ax.plot(time_perturb_2[:i2], val_perturb_zo[:i2], 'g--', label='Black-box ZO PGD')
     ax.plot(time_perturb_3[:i3], val_perturb_r[:i3], 'k--', label='White-box Riemannian')
     ax.plot(time_perturb_4[:i4], val_perturb_rzo[:i4], 'b:', label='Black-box ZO Riemannian')
     ax.set(xlabel='CPU time', ylabel='Loss value',
@@ -210,4 +257,3 @@ def main():
 
 if __name__== "__main__":
     main()
-
